4dcbae458b21abccc414a6ddb15cd41e2d54c697
mach-mahe <975465381@qq.com>
2021-03-09 11:07:06 +0800

    update：模板内容更新

--- generator/index.js
+++ generator/index.js
@@ -3,16 +3,16 @@ module.exports = (api, options, rootOptions) => {
     api.extendPackage({
         dependencies: {
 
-            "csmf-cli-helper": "^1.0.1"
+            "csmf-cli-helper": "^1.0.4"
         },
         devDependencies: {
-            "@mapbox/stylelint-processor-arbitrary-tags": "^0.2.0",
             "@vue/cli-plugin-babel": "~4.5.0",
             "@vue/cli-plugin-eslint": "~4.5.0",
             "@vue/cli-plugin-router": "~4.5.0",
             "@vue/cli-plugin-vuex": "~4.5.0",
             "@vue/cli-service": "~4.5.0",
             "@vue/eslint-config-prettier": "^6.0.0",
+            "axios-mock-adapter": "^1.19.0",
             "babel-eslint": "^10.1.0",
             "babel-plugin-lodash": "^3.3.4",
             "eslint": "^6.7.2",
@@ -21,26 +21,28 @@ module.exports = (api, options, rootOptions) => {
             "less": "^3.0.4",
             "less-loader": "^5.0.0",
             "lodash-webpack-plugin": "^0.11.6",
+            "mockjs": "^1.1.0",
             "prettier": "^1.19.1",
             "style-resources-loader": "^1.3.2",
-            "stylelint": "^8.4.0",
+            "stylelint": "^13.8.0",
             "stylelint-config-rational-order": "^0.1.2",
-            "stylelint-config-standard": "^18.1.0",
+            "stylelint-config-recommended": "^3.0.0",
+            "stylelint-config-standard": "^20.0.0",
             "stylelint-declaration-block-no-ignored-properties": "^2.3.0",
             "stylelint-order": "^4.1.0",
-            "vue-cli-plugin-style-resources-loader": "~0.1.4",
-            "vue-cli-plugin-stylelint": "0.0.2",
-            "vue-template-compiler": "^2.6.11",
+            "stylelint-webpack-plugin": "^2.1.1",
             "vue-auto-routing": "^0.5.0",
             "vue-cli-plugin-auto-routing": "~0.4.1",
+            "vue-cli-plugin-style-resources-loader": "~0.1.4",
+            "vue-template-compiler": "^2.6.11"
         }
     });
     // 默认的serve和build命令，执行之前，校验并修复文件
     api.extendPackage({
 
         scripts: {
-            "serve": "npm run eslint:fix && npm run prettier:format && npm run stylelint:fix && vue-cli-service serve",
-            "build": "npm run eslint:fix && npm run prettier:format && npm run stylelint:fix && vue-cli-service build",
+            "serve": "npm run eslint:fix && npm run prettier:format && vue-cli-service serve",
+            "build": "npm run eslint:fix && npm run prettier:format && vue-cli-service build",
             "eslint:fix": "eslint \"src/**/*.{vue,js}\" --fix",
             "prettier:format": "prettier --write \"src/**/*.{vue,js}\"",
             "stylelint:fix": "stylelint \"src/**/*.{vue,css,less}\" --fix"
